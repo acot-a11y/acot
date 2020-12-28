fb6e4bad5a5562260561c5585eb83ba492bdb6a6
wadackel <wadackel@gmail.com>
2020-12-28 19:50:37 +0900

    feat: add `@acot/mock` package

---
+++ packages/mock/README.md
@@ -0,0 +1,15 @@
+# @acot/mock
+
+acot mock modules.
+
+## Installation
+
+Install via npm:
+
+```bash
+$ npm install --save @acot/mock
+```
+
+## Usage
+
+_T.B.A_
---
+++ packages/mock/src/core.ts
@@ -0,0 +1,50 @@
+import type {
+  Core,
+  CoreEventMap,
+  TestDescriptor,
+  EventListener,
+  Summary,
+} from '@acot/types';
+import { createSummary } from '@acot/factory';
+import Emittery from 'emittery';
+
+export class MockCore implements Core {
+  public version = 'mock';
+  public cases: [path: string, descriptor: TestDescriptor][] = [];
+  public summary: Summary = createSummary();
+  public emitter = new Emittery();
+
+  public add(path: string, descriptor: TestDescriptor): void {
+    this.cases.push([path, descriptor]);
+  }
+
+  public async audit(): Promise<Summary> {
+    this.emitter.emit('launch:start', []);
+    this.emitter.emit('launch:complete', []);
+    this.emitter.emit('audit:start', []);
+    this.emitter.emit('audit:complete', []);
+    this.emitter.emit('test:start', []);
+    this.emitter.emit('test:complete', []);
+    await this.close();
+    return this.summary;
+  }
+
+  public async close(): Promise<void> {
+    this.emitter.emit('close:start', []);
+    this.emitter.emit('close:complete', []);
+  }
+
+  public on<T extends keyof CoreEventMap>(
+    eventName: T,
+    listener: EventListener<CoreEventMap, T>,
+  ): void {
+    this.emitter.on(eventName as any, listener as any);
+  }
+
+  public off<T extends keyof CoreEventMap>(
+    eventName: T,
+    listener?: EventListener<CoreEventMap, T>,
+  ): void {
+    this.emitter.off(eventName as any, listener as any);
+  }
+}
